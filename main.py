1db07cf44f24438f84709b66ad9193bbb5ca10e9
innocuous_rift <alexandra.rose.hanson@gmail.com>
2019-04-13 11:30:27 -0700

    Style sheet and landing page for Take a Bike (fonts and centering)

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 # [END create_app]
 
+# [START home]
+@app.route('/')
+def home():
+    return render_template('index.html')
+# [END home]
 
 # [START form]
 @app.route('/form')
